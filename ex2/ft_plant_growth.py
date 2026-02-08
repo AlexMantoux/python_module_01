1859cd2ca382b072eebd12f638d8c07734efb8ba
AlexMantoux <alex.mantoux@gmail.com>
2026-02-08 16:01:31 +0100

    Modify ex2

--- ex2/ft_plant_growth.py
+++ ex2/ft_plant_growth.py
@@ -1,17 +1,53 @@
 class Plant:
-    def __init__(self, name, height, age):
-        self.name = name
-        self.height = height
-        self.age = age
+    """
+    Represents a plant with a name, height, and age.
 
-    def grow(self, n):
+    Attributes
+    ----------
+    name : str
+        The name of the plant.
+    height : int
+        The height of the plant in centimeters.
+    age : int
+        The age of the plant in days.
+    """
+    def __init__(self, name: str, height: int, age: int) -> None:
+        self.name: str = name
+        self.height: int = height
+        self.age: int = age
+
+    def grow(self, n: int) -> None:
+        """
+        Increase the plant's height by a given amount.
+
+        Parameters
+        ----------
+        n : int
+            The number of centimeters by which the plant should grow.
+        """
         self.height += n
 
-    def increase_age(self, n):
+    def increase_age(self, n) -> None:
+        """
+        Increase the plant's age by a given number of days.
+
+        Parameters
+        ----------
+        n : int
+            The number of days to add to the plant's age.
+        """
         self.age += n
 
 
-def get_info(plant):
+def get_info(plant) -> None:
+    """
+    Display detailed information about a plant, including its growth this week.
+
+    Parameters
+    ----------
+    plant : Plant
+        The Plant object whose information is to be displayed.
+    """
     print(plant.name + ": ",
           plant.height, "cm, ", plant.age, " days old", sep="")
     print("Growth this week: +", 6, "cm", sep="")
