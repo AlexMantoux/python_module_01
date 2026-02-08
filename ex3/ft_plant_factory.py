329cd4298c1b9f7736725c5343046d615a4ce81a
AlexMantoux <alex.mantoux@gmail.com>
2026-02-08 21:24:13 +0100

    Modify ex3

--- ex3/ft_plant_factory.py
+++ ex3/ft_plant_factory.py
@@ -1,25 +1,107 @@
 class Plant:
-    def __init__(self, name, height, age):
-        self.name = name
-        self.height = height
-        self.age = age
+    """
+    Represents a plant with a name, height, and age.
+
+    Attributes
+    ----------
+    name : str
+        The name of the plant.
+    height : int
+        The height of the plant in centimeters.
+    age : int
+        The age of the plant in days.
+    """
+
+    def __init__(self, name: str, height: int, age: int) -> None:
+        """
+        Initialize the plant with name, age, and height.
+
+        Args:
+            name (str): The name of the plant.
+            age (int): The age of the plant in days.
+            height (int): The height of the plant in cm.
+        """
+        self.name: str = name
+        self.height: int = height
+        self.age_days: int = age
+        self.last_height: int = height
+        self.last_age_days: int = age
+        print(f"Created: {self.name} ({self.height}cm, {self.age_days} days)")
+
+    def display(self):
+        """
+        Display the plant's details.
+        """
+        print(f"{self.name.title()}: {self.height}cm,\
+ {self.age_days} days old")
+
+    def grow(self, size: int) -> None:
+        """
+        Increase the plant's height by a given amount.
+
+        Parameters
+        ----------
+        size : int
+            The number of centimeters by which the plant should grow.
+        """
+        self.last_height = self.height
+        self.height += size
+
+    def age(self, days: int) -> None:
+        """
+        Increase the plant's age by a given number of days.
+
+        Parameters
+        ----------
+        n : int
+            The number of days to add to the plant's age.
+        """
+        self.last_age_days = self.age_days
+        self.age_days += days
+
+    def get_info(self) -> None:
+        """
+        Display detailed information about a plant, including its growth
+        this week.
+
+        Parameters
+        ----------
+        plant : Plant
+            The Plant object whose information is to be displayed.
+        """
+        print(f"Growth this week: +{self.height - self.last_height}cm")
 
 
-def display_plant_created(plants):
-    count = 0
-    for plant in plants:
-        count += 1
-        print("Created: " + plant.name + " (", plant.height, " cm, ",
-              plant.age, " days)", sep="")
-    print("\nTotal plants created:", count)
+class PlantFactory:
+    """
+    A class to represent q factory for create plant.
+    """
+    factoryPlant = 0
+
+    def create_plant(val: tuple[str, int, int]) -> Plant:
+        """
+        Create lot of plants
+
+        Args:
+            val tuple[str, int, int]: Table of values
+        """
+        PlantFactory.factoryPlant += 1
+        name, size, age = val
+        return Plant(name, size, age)
 
 
 if __name__ == "__main__":
-    rose = Plant("Rose", 25, 30)
-    oak = Plant("Oak", 200, 365)
-    cactus = Plant("Cactus", 5, 90)
-    sunflower = Plant("Sunflower", 80, 45)
-    fern = Plant("Fern", 15, 120)
-    plant_list = [rose, oak, cactus, sunflower, fern]
+    plants_tuple = [
+            ("Rose", 25, 30),
+            ("Oak", 200, 365),
+            ("Cactus", 5, 90),
+            ("Sunflower", 80, 45),
+            ("Fern", 15, 120)
+        ]
     print("=== Plant Factory Output ===")
-    display_plant_created(plant_list)
+
+    plants = []
+    for plant in plants_tuple:
+        plants.append(PlantFactory.create_plant(plant))
+    print()
+    print(f"Total plants created: {PlantFactory.factoryPlant}")
