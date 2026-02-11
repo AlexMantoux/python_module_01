d77de71104331ed709a144d97d051cf85f5f9cba
AlexMantoux <alex.mantoux@gmail.com>
2026-02-11 22:08:51 +0100

    Modify ex6

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -20,41 +20,97 @@ class Plant:
         """
         self.name: str = name
         self.height: int = height
+    
+    def grow(self, growValue : int):
+        self.height += growValue
+        print(f"{self.name} grew {growValue}cm")
+
+    
 
 
 class FloweringPlant(Plant):
-    pass
+    
+    def __init__(self, name : str, height : int, color : str) -> None:
+        super().__init__(name, height)
+        self.color : str = color
 
 
-class PriweFlower(FloweringPlant):
-    pass
+
+class PrizeFlower(FloweringPlant):
+
+    def __init__(self, name : str, height : int, color : str, prize_point : int) -> None:
+        super().__init__(name, height, color)
+        self.prize_point : int = prize_point
 
 
 class Garden:
     def __init__(self, name: str) -> None:
         self.name: str = name
-        self.garden: list[Plant] = []
+        self.plants: list[Plant | FloweringPlant | PrizeFlower] = []
 
     def add_plant(self, plant: Plant) -> None:
-        self.garden.append(plant)
+        self.plants.append(plant)
         print(f"Added {plant.name.capitalize()} to {self.name}'s garden")
 
-    def display_garden(self):
+    def display_garden(self) -> None:
         for plant in self.garden:
             print(f"{plant}")
 
+    def help_plant(self, growValue : int) -> None:
+        print(f"{self.name} is helping all plants grow...")
+        for plant in self.plants:
+            plant.grow(growValue)
+
+    def garden_report(self):
+        print(f"=== {self.name.capitalize()}'s Garden Report ===")
+        print("Plants in garden:")
+        for plant in self.plants:
+            print(f"- {plant.name.capitalize()}: {plant.height}cm")
+
 
 class GardenManager:
-    pass
+
+    gardens : list[Garden] = []
+    handleGardens : int = 0
+
+    class GardenStats:
+
+        def __init__(self, plants : list[Plant]):
+            self.plants = plants
+        
+        def plant_type(self) -> dict[str, int]:
+            types_count : dict[str, int] = {"regular": 0, "flowering": 0, "Prize": 0}
+            for plant in self.plants:
+                if isinstance(plant, Plant):
+                    types_count["regular"] += 1
+                if isinstance(plant, FloweringPlant):
+                    types_count["flowering"] += 1
+                else:
+                    types_count["prize"] += 1
+            return types_count
+
+    @staticmethod
+    def stats_display(self):
+        ...
+
+    @classmethod
+    def create_garden_network(cls, gardenList : list[Garden]):
+        for garden in gardenList:
+            cls.handleGardens += 1
+            cls.gardens.append(garden)
 
 
 def main() -> None:
     print("=== Garden Management System Demo ===")
     print()
     alice = Garden("Alice")
-    alice.add_plant(Plant("oak tree", 25))
-    alice.add_plant(Plant("rose", 25))
-    alice.add_plant(Plant("sunflower", 25))
+    alice.add_plant(Plant("oak tree", 100))
+    alice.add_plant(FloweringPlant("rose", 25, "red"))
+    alice.add_plant(PrizeFlower("sunflower", 50, "yellow", 10))
+    print()
+    alice.help_plant(1)
+    print()
+    alice.garden_report()
 
 
 if __name__ == "__main__":
