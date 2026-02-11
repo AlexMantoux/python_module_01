2b5e596d12fa930742b4cc0d54ed3a61ad92fd51
AlexMantoux <alex.mantoux@gmail.com>
2026-02-11 19:41:14 +0100

    Modify ex6

--- ex6/ft_garden_analytics.py
+++ ex6/ft_garden_analytics.py
@@ -1,6 +1,6 @@
 class Plant:
     """
-    Represents a plant with a name, height, and age.
+    Represents a plant with a name and height
 
     Attributes
     ----------
@@ -8,190 +8,54 @@ class Plant:
         The name of the plant.
     height : int
         The height of the plant in centimeters.
-    age : int
-        The age of the plant in days.
     """
 
-    def __init__(self, name: str, height: int, age: int) -> None:
+    def __init__(self, name: str, height: int) -> None:
         """
-        Initialize the plant with name, age, and height.
+        Initialize the plant with name and height.
 
         Args:
             name (str): The name of the plant.
-            age (int): The age of the plant in days.
             height (int): The height of the plant in cm.
         """
         self.name: str = name
         self.height: int = height
-        self.age_days: int = age
-
-    def display(self) -> None:
-        """
-        Display the plant's details.
-        """
-        print(f"{self.height}cm, {self.age_days} days old", end="")
 
 
-class Flower(Plant):
-    """
-    Represents a flower with a name, height, age and color. 
+class FloweringPlant(Plant):
+    pass
 
-    Attributes
-    ----------
-    name : str
-        The name of the Flower.
-    height : int
-        The height of the Flower in centimeters.
-    age : int
-        The age of the Flower in days.
-    color : str
-        The color of the Flower.
-    """
-    def __init__(self, name: str, height: int, age: int, color: str) -> None:
-        """
-        Initialize the Flower with name, height, age and color.
 
-        Args:
-            name (str): The name of the Flower.
-            height (int): The height of the Flower in cm.
-            age (int): The age of the Flower in days.
-            color (str): The color of the flower.
-        """
-        super().__init__(name, height, age)
-        self.color = color
-
-    def display(self) -> None:
-        """
-        Display the plant's details.
-        """
-        print(f"{self.name} (Flower): ", end="")
-        super().display()
-        print(f", {self.color} color")
-
-    def bloom(self) -> None:
-        """
-        simulate and display flower blooming
-        """
-        print(f"{self.name} is blooming beautifully!")
-    
-class Tree(Plant):
-    """
-    Represents a tree with a name, height, age and trunk_diameter. 
-
-    Attributes
-    ----------
-    name : str
-        The name of the tree.
-    height : int
-        The height of the tree in centimeters.
-    age : int
-        The age of the tree in days.
-    trunk_diameter : int
-        The trunk diameter of the tree.
-    """
-    def __init__(self, name: str, height: int, age: int, trunk_diameter: int) -> None:
-        """
-        Initialize the tree with name, height, age and trunk diameter.
-
-        Args:
-            name (str): The name of the tree.
-            height (int): The height of the tree in cm.
-            age (int): The age of the tree in days.
-            trunk_diameter (int): The trunk diameter of the tree.
-        """
-        super().__init__(name, height, age)
-        self.trunk_diameter = trunk_diameter
-
-    def display(self) -> None:
-        """
-        Display the plant's details.
-        """
-        print(f"{self.name} (Tree): ", end="")
-        super().display()
-        print(f", {self.trunk_diameter}cm diameter")
-
-    def produce_shade(self) -> None:
-        """
-        Compute and display the shade provide by the tree
-        """
-        print(f"{self.name} provides {int(self.height * 0.156)} square meters of shade")
-
-class Vegetable(Plant):
-    """
-    Represents a vegetable with a name, height, age, harvest season and nutritional value. 
-
-    Attributes
-    ----------
-    name : str
-        The name of the vegetable.
-    height : int
-        The height of the vegetable in centimeters.
-    age : int
-        The age of the vegetable in days.
-    harvest_season : str
-        The harvest season of the vegetable.
-    nutritional_value : str
-        The nutritional value of the vegetable.
-    """
-    def __init__(self, name: str, height: int, age: int, harvest_season: str, nutritional_value: str) -> None:
-        """
-        Initialize the vegetable with name, height, age, harvest season and nutritional value.
-
-        Args:
-            name (str): The name of the vegetable.
-            height (int): The height of the vegetable in cm.
-            age (int): The age of the vegetable in days.
-            harvest_season (str): The harvest season of the vegetable.
-            nutritional_value (str): The nutritional value of the vegetable.
-        """
-        super().__init__(name, height, age)
-        self.harvest_season = harvest_season
-        self.nutritional_value = nutritional_value
-
-    def display(self) -> None:
-        """
-        Display the plant's details.
-        """
-        print(f"{self.name} (Vegetable): ", end="")
-        super().display()
-        print(f", {self.harvest_season} harvest")
-    
-    def get_nutri_info(self) -> None:
-        """
-        Display nutritional value
-        """
-        print(f"{self.name} is rich in {self.nutritional_value}")
+class PriweFlower(FloweringPlant):
+    pass
 
 
 class Garden:
-
     def __init__(self, name: str) -> None:
         self.name: str = name
         self.garden: list[Plant] = []
 
     def add_plant(self, plant: Plant) -> None:
         self.garden.append(plant)
-        print(f"Added {plant.name} to {self.name}'s garden")
+        print(f"Added {plant.name.capitalize()} to {self.name}'s garden")
 
     def display_garden(self):
         for plant in self.garden:
             print(f"{plant}")
 
+
 class GardenManager:
+    pass
 
-    def __init__(self, garden: Garden):
-        self.garden_list: list[Garden] = []
 
-if __name__ == "__main__":
-
-    rose = Flower("Rose", 25, 30, "red")
-    oak = Tree("Oak tree", 500, 1850, 50)
-    tomato = Vegetable("Tomato", 80, 90, "summer", "vitamin C")
-    alice = GardenManager("Alice")
-
+def main() -> None:
     print("=== Garden Management System Demo ===")
     print()
+    alice = Garden("Alice")
+    alice.add_plant(Plant("oak tree", 25))
+    alice.add_plant(Plant("rose", 25))
+    alice.add_plant(Plant("sunflower", 25))
 
-    alice.add_plant(rose)
-    alice.add_plant(oak)
-    alice.add_plant(tomato)
+
+if __name__ == "__main__":
+    main()
